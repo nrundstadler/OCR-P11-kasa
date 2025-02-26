fce32c9314fa7f725f04e7ac26aa942b6dbf70f4
Nicolas Rundstadler <n.rundstadler@laposte.net>
2025-02-26 17:52:45 +0100

    Install Sass and integrate initial SCSS files

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,12 @@
-import "./App.css";
-
 function App() {
   return (
     <>
       <h1>Kasa</h1>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Assumenda quidem saepe in expedita corrupti laborum
+        excepturi temporibus, incidunt impedit quae itaque molestias fugit odit nemo voluptate voluptatum dignissimos
+        alias. Quae?
+      </p>
     </>
   );
 }
